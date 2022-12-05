ca0d810b46e2ee513ae9060678b964c0aa712dfd
tomho <thingasd@gmail.com>
2022-12-05 23:55:05 +0800

    reinit

--- server.js
+++ server.js
@@ -4,50 +4,16 @@ const fastify = require('fastify')({
 const PORT = process.env.port || 3000;
 require('dotenv').config();
 const axios = require('axios').default;
-const TeleBot = require('telebot');
+const { Telegraf } = require('telegraf');
+const { message } = require('telegraf/filters');
 
-fastify.register(require('./routes/default-route'));
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
 
-
-const initBot = () => {
-    const bot = new TeleBot(`${process.env.TELEGRAM_BOT_TOKEN}`);
-    return bot;
-}
-
-const initBotMethods = (bot) => {
-    bot.on(['/start', '/hello'], (msg) => {
-        return msg.reply.text('Welcome!');
-    });
-
-    bot.on(/(tell\s)?joke*/, async (msg) => {
-        const result = await axios.get('https://official-joke-api.appspot.com/random_joke');
-        if (result?.data) {
-            return msg.reply.text(`${result.data.setup}\n\n ---${result.data.punchline}`);
-        } else {
-            return msg.reply.text("Let's retry that!");
-        }
-    });
-
-    bot.on(['@NoMoreJBot'], (msg) => {
-        return msg.reply.text('收皮啦撚樣');
-    });
-
-}
-
-const startBot = () => {
-    const bot = initBot();
-    initBotMethods(bot);
-    bot.start();
-}
-
-const start = async () => {
-    try {
-        await fastify.listen(PORT);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-}
-
-startBot();
-start();
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
